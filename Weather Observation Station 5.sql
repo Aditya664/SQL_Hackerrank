9f0e93cbc70dcaac5a9ecfc0599eabbec74d3c82
Restyled.io <commits@restyled.io>
2022-07-26 06:43:45 +0000

    Restyled by pg_format

--- Weather Observation Station 5.sql
+++ Weather Observation Station 5.sql
@@ -1,10 +1,22 @@
 /*
 Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
-*/
+ */
+SELECT
+    TOP 1 CITY,
+    LEN (CITY) AS Ln
+FROM
+    STATION
+ORDER BY
+    Ln ASC,
+    CITY ASC;
 
-SELECT TOP 1 CITY, LEN(CITY) AS Ln FROM STATION
-ORDER BY Ln ASC, CITY ASC;
+SELECT
+    TOP 1 CITY,
+    LEN (CITY) AS Ln
+FROM
+    STATION
+ORDER BY
+    Ln DESC,
+    CITY ASC;
 
-SELECT TOP 1 CITY, LEN(CITY) AS Ln FROM STATION
-ORDER BY Ln DESC, CITY ASC;
