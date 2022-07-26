07642ca60ec3049d10e866c9494133483a815759
Restyled.io <commits@restyled.io>
2022-07-26 06:32:30 +0000

    Restyled by pg_format

--- Select By ID.sql
+++ Select By ID.sql
@@ -1,6 +1,11 @@
 /*
 Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
-*/
-SELECT * FROM CITY 
-WHERE ID = 1661;
+ */
+SELECT
+    *
+FROM
+    CITY
+WHERE
+    ID = 1661;
+
